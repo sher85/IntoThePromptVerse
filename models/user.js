63f8badba391136c89492d5d0400f8bb8e2da85f
sher85 <32082005+sher85@users.noreply.github.com>
2023-05-27 17:35:22 -0500

    oauth fixed

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
         type: String,
         unique: [true, 'Username already exists!'],
         required: [true, 'Username is required!'],
-        mathc: [
+        match: [
             /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
             'Username invalid, it should contain 8-20 alphanumeric letters and be unique!',
         ],
