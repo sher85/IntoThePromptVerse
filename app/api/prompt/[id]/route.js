8442d67acbbf43a26fee36a49b5fda46733d355c
sher85 <32082005+sher85@users.noreply.github.com>
2023-06-14 22:48:08 -0500

    Delete prompt route added

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -40,3 +40,14 @@ export const PATCH = async (request, { params }) => {
 };
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndDelete(params.id);
+
+        return new Response('Prompt deleted successfully', { status: 200 });
+    } catch (error) {
+        return new Response('Failed to delete prompt', { status: 500 });
+    }
+};
